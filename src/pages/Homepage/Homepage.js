272ee8a183f78bd8a67abcce0bd732fba3d1631a
unknown <alpinmohsa541>
2022-11-23 20:00:44 +0700

    removing removing unimportant imports

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { AiFillNotification } from 'react-icons/ai'
 
 export default function Homepage() {
   return (
-    <div style={{fontSize: '5rem'}}><AiFillNotification /> Notification</div>
+    <h1>Hallo</h1>
+    
   )
 }
