7ad48492684c25f6c615f5aaefcd03765abfe03d
Sae Pasomba <saepasomba12@gmail.com>
2022-11-25 14:23:46 +0700

    Add yup and implement react-hook-form to login form

--- src/pages/Homepage/LoginModal.js
+++ src/pages/Homepage/LoginModal.js
@@ -1,5 +1,13 @@
-import { Button, Flex, FormControl, FormLabel, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Flex, FormControl, FormErrorMessage, FormLabel, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const loginSchema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().min(5).required()
+})
 
 export default function LoginModal(props) {
   const {
@@ -7,41 +15,59 @@ export default function LoginModal(props) {
     onOpen,
     onClose
   } = props;
+
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm(
+    {
+      mode: 'onChange',
+      resolver: yupResolver(loginSchema)
+    }
+  )
     
+  const loginSubmit = (data) => {
+    console.log('Login clicked')
+    console.log(data)
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
 
+        <form onSubmit={handleSubmit(loginSubmit)}>
         <ModalHeader>Login to your account!</ModalHeader>
 
         <ModalBody>
-        <form>
             <Flex flexDir='column' gap='1rem'>
-            <FormControl isRequired>
+            <FormControl isRequired isInvalid={errors.email}>
               <FormLabel>Email</FormLabel>
-              <Input placeholder='Email...' />
+              <Input {...register('email')} placeholder='Email...' />
+              <FormErrorMessage>
+                {errors.email && errors.email.message}
+              </FormErrorMessage>
             </FormControl>
 
-            <FormControl isRequired>
+            <FormControl isRequired isInvalid={errors.password}>
               <FormLabel>Password</FormLabel>
-              <Input placeholder='Password...' />
+              <Input {...register('password')} type='password' placeholder='Password...'/>
+              <FormErrorMessage>
+                {errors.password && errors.password.message}
+              </FormErrorMessage>
             </FormControl>
             </Flex>
-          </form>
         </ModalBody>
 
         <ModalFooter>
           <Flex gap='1rem'>
-            <Button>
+            <Button onClick={onClose} >
               Cancel
             </Button>
-            <Button colorScheme='blue'>
+            <Button type='submit' colorScheme='blue'>
               Login
             </Button>
           </Flex>
         </ModalFooter>
 
+          </form>
       </ModalContent>
     </Modal>
   )
