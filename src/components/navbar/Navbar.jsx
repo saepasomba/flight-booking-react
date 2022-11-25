e666127f34dba3906cf335d464abc063fec3a427
MhassanS23 <maulanahassan366@gmail.com>
2022-11-25 20:49:29 +0700

    Merubah design responsive mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,8 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 import * as React from 'react'
-import { FiMenu,FiChevronDown } from 'react-icons/fi'
+import { FiMenu,FiChevronDown, FiLogIn  } from 'react-icons/fi'
+import {CgLogOut} from 'react-icons/cg'
 import {IoIosNotificationsOutline} from 'react-icons/io'
 import {BsBookmarkCheckFill} from 'react-icons/bs'
 import {MdOutlinePayment} from 'react-icons/md'
@@ -57,8 +58,8 @@ export const App = () => {
                     <Button color='#ffffff' key={item}>{item}</Button>
                   ))}
 
-                  <Menu px={'var(--chakra-space-2)'}>
-                    <MenuButton as={Button} leftIcon={<IoIosNotificationsOutline color='#ffffff' size='1.5rem' me={'0.05rem'} />} rightIcon={<FiChevronDown color='#ffffff' />}>
+                  <Menu>
+                    <MenuButton as={Button} leftIcon={<IoIosNotificationsOutline color='#ffffff' size='1.5rem' me={'0.05rem'} />} rightIcon={<FiChevronDown color='#ffffff'/>}>
                     </MenuButton>
                     <MenuList color='black' maxW={'var(--chakra-sizes-container-sm)'}>
                       <MenuItem minH='48px'>                  
@@ -77,31 +78,48 @@ export const App = () => {
                 </HStack>
               </Flex>
             ) : (
-              <Menu>
-                <MenuButton
-                  as={IconButton}
-                  aria-label='Options'
-                  icon={<FiMenu />}
-                  variant='outline'
-                />
-                <MenuList color='black' fontSize='1.5rem'>
-                  <MenuItem icon={<FaHome />}>
-                    Homepage
-                  </MenuItem>
-                  <MenuItem icon={<BsBookmarkCheckFill />}>
-                    Booked List
-                  </MenuItem>
-                  <MenuItem icon={<MdOutlinePayment />}>
-                    Payment
-                  </MenuItem>
-                  <MenuItem icon={<FcAbout color='black' />}>
-                    About Us
-                  </MenuItem>
-                  <MenuItem icon={<IoIosNotificationsOutline />}>
-                    Notification
-                  </MenuItem>
-                </MenuList>
-              </Menu>
+              <Flex gap={'0.5rem'}>
+                <Menu spacing="3">
+                    <MenuButton as={Button} colorScheme='white' variant='outline' px={'var(--chakra-space-2)'} leftIcon={<IoIosNotificationsOutline color='#ffffff' size='1.5rem' me={'0rem'} />} rightIcon={<FiChevronDown color='#ffffff' ms={'0rem'} />}>
+                    </MenuButton>
+                    <MenuList color='black' maxW={'var(--chakra-sizes-80)'} minW={'var(--chakra-sizes-64)'}>
+                      <MenuItem minH='48px'>                  
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto natus eius laudantium accusantium unde et quia, hic quos voluptates cupiditate facilis fugiat inventore accusamus. Blanditiis cum sed repellendus labore doloribus.</span>
+                      </MenuItem>
+                      <MenuItem minH='40px'>
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit vero, unde molestiae minus eos aliquid, iste voluptate consectetur, ducimus quibusdam saepe similique consequuntur fugit reiciendis ipsa sint fuga earum dolore.</span>
+                      </MenuItem>
+                    </MenuList>
+                </Menu>
+                <Menu>
+                  <MenuButton
+                    as={IconButton}
+                    aria-label='Options'
+                    icon={<FiMenu />}
+                    variant='outline'
+                  />
+                  <MenuList color='black' fontSize='1.5rem'>
+                    <MenuItem icon={<FaHome />}>
+                      Homepage
+                    </MenuItem>
+                    <MenuItem icon={<BsBookmarkCheckFill />}>
+                      Booked List
+                    </MenuItem>
+                    <MenuItem icon={<MdOutlinePayment />}>
+                      Payment
+                    </MenuItem>
+                    <MenuItem icon={<FcAbout color='black' />}>
+                      About Us
+                    </MenuItem>
+                    <MenuItem icon={<FiLogIn color='black' />}>
+                      Login
+                    </MenuItem>
+                    <MenuItem icon={<CgLogOut color='black' />}>
+                      Logout
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Flex>
             )}
           </HStack>
         </Container>
