2793eb55e0b30abbf050ea79172f0957a32d7fdb
unknown <alpinmohsa541>
2022-11-26 14:58:15 +0700

    Memperbaiki penulisan pada index.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -41,7 +41,7 @@ const Header = ({ type }) => {
   };
 
   const handleSearch = () => {
-    navigate("/hotels", { state: { destination, date, options } });
+    navigate("/", { state: { destination, date, options } });
   };
 
   return (
