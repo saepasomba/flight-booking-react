05568360e45b001e78c6e8c7478ce4eb648726ec
MhassanS23 <maulanahassan366@gmail.com>
2022-11-28 19:41:47 +0700

    make footer with responsive

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,102 @@
+import {
+    Button,
+    ButtonGroup,
+    Container,
+    Divider,
+    IconButton,
+    Stack,
+    Text,
+  } from '@chakra-ui/react'
+  import * as React from 'react'
+  import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+  
+  export const Footers = () => (
+    <Container as="footer" role="contentinfo" bg="#063970" maxW={'100%'} color="white" >
+      <Stack
+        spacing="8"
+        direction={{
+          base: 'column',
+          md: 'row',
+        }}
+        justify="space-between"
+        py={{
+          base: '12',
+          md: '16',
+        }}
+      >
+        <Stack
+          spacing={{
+            base: '6',
+            md: '8',
+          }}
+          align="start"
+        >
+          <Text color="muted">Thanks for choose us for your activity.</Text>
+          <Text fontSize='3rem' fontFamily='cursive'>
+            <h1>BinarFly</h1>
+          </Text>
+        </Stack>
+        <Stack
+          direction={{
+            base: 'column',
+            md: 'column',
+            lg: 'row',
+          }}
+          spacing={{
+            base: '12',
+            md: '8',
+          }}
+        >
+          <Stack direction="row" spacing="8">
+            <Stack spacing="3"  minW="36" flex="1">
+              <Text fontSize="md" fontWeight="bold" color="subtle">
+                Contact Info
+              </Text>
+              <Text fontSize="md" fontWeight="semibold" color="grey">
+                Jalan Suroyo No. 161 Mayangan Kota Probolinggo 672000
+              </Text>
+              <Text fontSize="md" fontWeight="semibold" color="grey">
+                binarfly@gmail.com
+              </Text>
+              <Text fontSize="md" fontWeight="semibold" color="grey">
+                081-233-343-808
+              </Text>
+            </Stack>
+            <Stack spacing="4" minW="36" flex="1">
+              <Stack spacing="3" shouldWrapChildren fontSize="md" fontWeight="semibold" color="subtle">
+                <Button variant="link" color="white">Our Service</Button>
+                <Button variant="link" color="white">Why Us</Button>
+                <Button variant="link" color="white">Testimonial</Button>
+                <Button variant="link" color="white">FAQ</Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+      <Divider />
+      <Stack
+        pt="8"
+        pb="12"
+        justify="center"
+        direction={{
+          md: 'column',
+        }}
+        align="center"
+        flexDirection="column"
+      >
+        <ButtonGroup variant="ghost">
+          <IconButton
+            as="a"
+            href="#"
+            aria-label="LinkedIn"
+            icon={<FaLinkedin fontSize="1.25rem" />}
+          />
+          <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub fontSize="1.25rem" />} />
+          <IconButton as="a" href="#" aria-label="Twitter" icon={<FaTwitter fontSize="1.25rem" />} />
+        </ButtonGroup>
+        <Text fontSize="sm" color="subtle">
+          &copy; {new Date().getFullYear()} BinarFLy, Inc. All rights reserved.
+        </Text>
+      </Stack>
+    </Container>
+  )
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,12 +1,16 @@
 import { Button } from '@chakra-ui/react'
 import React from 'react'
-import {App} from '../../components/navbar/Navbar.jsx'
+import {Navbars} from '../../components/navbar/Navbar.jsx'
+import {Footers} from '../../components/footer/Footer.jsx'
 
 export default function Homepage() {
   return (
     <>
-    <App/>
+    <Navbars/>
     <h1>ini homepage</h1>
+    <p>loremLorem ipsum dolor sit amet consectetur adipisicing elit. Architecto natus eius laudantium accusantium unde et quia, hic quos voluptates cupiditate facilis fugiat inventore accusamus. Blanditiis cum sed repellendus labore doloribus.</p>
+    <p>loremLorem ipsum dolor sit amet consectetur adipisicing elit. Architecto natus eius laudantium accusantium unde et quia, hic quos voluptates cupiditate facilis fugiat inventore accusamus. Blanditiis cum sed repellendus labore doloribus.</p>
+    <p>loremLorem ipsum dolor sit amet consectetur adipisicing elit. Architecto natus eius laudantium accusantium unde et quia, hic quos voluptates cupiditate facilis fugiat inventore accusamus. Blanditiis cum sed repellendus labore doloribus.</p>
       <Button>
         Login
       </Button>
@@ -14,6 +18,7 @@ export default function Homepage() {
       <Button>
         Register
       </Button>
+    <Footers/>
     </>
   )
 }
