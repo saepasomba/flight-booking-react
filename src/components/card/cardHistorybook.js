57a51b018a00a641a04d9027fdac2df8cb248c1a
unknown <alpinmohsa541>
2022-12-12 20:55:59 +0700

    Change design

--- src/components/card/cardHistorybook.js
+++ src/components/card/cardHistorybook.js
@@ -16,39 +16,39 @@ import {MdAirplanemodeActive} from 'react-icons/md'
 export default function CardHistorybook() {
 
     return(
-    <Card padding='0' width={['95%','80%', '80%', '75%']} bg='white' border='1px solid whitesmoke' alignSelf='center' borderRadius='var(--chakra-radii-xl)' mb='1rem' size='sm'>
+    <Card  padding='0' width={['95%','80%', '80%', '75%']} bg='white' border='1px solid #0D4C92 ' alignSelf='center' borderRadius='var(--chakra-radii-xl)' mb='1rem' size='sm'>
 
-        <CardHeader bgGradient='linear(170deg, black, #063970, black)' color='white' borderTopLeftRadius='var(--chakra-radii-xl)' borderTopRightRadius='var(--chakra-radii-xl)' pb='3'>
+        <CardHeader color='white' borderTopLeftRadius='var(--chakra-radii-xl)' borderTopRightRadius='var(--chakra-radii-xl)' pb='3'>
             <Flex flexDirection={['column','column','row', 'row']} justifyContent={['center','center','space-between','space-between']}>
-                    <Heading textAlign={['start','start','null','null']} size={['sm','sm','md','md']}  mb={['1','1','0','0']}>INV-MPW-221209-13824</Heading>
-                    <Text textAlign='start'>Rp1820000</Text>
+                    <Heading color='black' textAlign={['start','start','null','null']} size={['sm','sm','md','md']}  mb={['1','1','0','0']}>INV-MPW-221209-13824</Heading>
+                    <Text color='black' textAlign='start'>Rp1820000</Text>
             </Flex>
         </CardHeader>
 
-        <CardBody padding='4' color='white' bg='#3579e6'>
+        <CardBody padding='4' color='white' bg='#D9D9D9'>
             <Flex flexDirection={['column','column','row', 'row']} justifyContent={['center','center','space-between','space-between']} mb='1rem'>
-                <Text>Booked By:    Tn. Riko</Text>
-                <Text>Total Person: 2 Person</Text>
+                <Text color='black'>Booked By:    Tn. Riko</Text>
+                <Text color='black'>Total Person: 2 Person</Text>
             </Flex>
             <Flex gap='3' mb='0.5rem' >
                 <MdAirplanemodeActive size='1.5rem' color='black'/>
-                <Text>Destination: Surabaya - Jakarta</Text>
+                <Text color='black'>Destination: Surabaya - Jakarta</Text>
             </Flex>
             <Flex gap='3' mb='0.5rem'>
                 <FaCalendarAlt size='1.5rem' color='black'/>
-                <Text>2022-12-09 13:08:24</Text>
+                <Text color='black'>2022-12-09 13:08:24</Text>
             </Flex>
             <Flex gap='3' mb='0.5rem'>
                 <GiAchievement size='1.5rem' color='black'/>
-                <Text >Business Class</Text>
+                <Text color='black'>Business Class</Text>
             </Flex>
             
         </CardBody>
 
         <Divider color='white'/>
-            <Flex flexDirection={['column','column','row', 'row']} justifyContent={['center','center','space-between','space-between']} padding='1rem' color='white'  alignItems={['flex-end','flex-end','center','center']} bgGradient='linear(170deg, black, #063970, black)' borderBottomLeftRadius='var(--chakra-radii-xl)' borderBottomRightRadius='var(--chakra-radii-xl)'>
-                <Text>Payment</Text>
-                <Text>Transfer Bank BCA</Text>
+            <Flex flexDirection={['column','column','row', 'row']} justifyContent={['center','center','space-between','space-between']} padding='1rem' color='white'  alignItems={['flex-end','flex-end','center','center']}  borderBottomLeftRadius='var(--chakra-radii-xl)' borderBottomRightRadius='var(--chakra-radii-xl)'>
+                <Text color='black'>Payment</Text>
+                <Text color='black'>Transfer Bank BCA</Text>
             </Flex>
     </Card>
     )
--- src/pages/HistoryBook/HistoryBookPage.js
+++ src/pages/HistoryBook/HistoryBookPage.js
@@ -8,37 +8,40 @@ import {
 } from '@chakra-ui/react'
 import CardHistorybook from "../../components/card/cardHistorybook";
 import CardSearchs from "../../components/card/cardSearch";
-import wave1 from '../../pages/Homepage/wave1.svg'
-import wave3 from '../../pages/Homepage/wave3.svg'
+import wave from '../../pages/Homepage/wave.svg'
+import wave2 from '../../pages/Homepage/wave2.svg'
 import {TiArrowBack} from 'react-icons/ti'
 import { Link } from "react-router-dom";
+import Navbar from '../../components/navbar/Navbar'
 
 export default function HistoryBookPage() {
   return (
   <Box>
-    <Box >
-        <Image src={wave1} widht='100%' pos='absolute'/>
-        <Image src={wave3} widht='100%'/>
-    </Box>
+    <Navbar/>
 
     <Flex flexDirection='column'>
         <Box pl={['0.5rem', '0.5rem', '3rem', '3rem']} alignSelf='flex-start'>
         <Link to="/" >
-            <Button  size={['sm', 'sm', 'md', 'md']} leftIcon={<TiArrowBack />} color='white' bg='#063970' variant='solid' mb='1rem'>
+            <Button  size={['sm', 'sm', 'md', 'md']} leftIcon={<TiArrowBack />} color='white' bg='#063970' variant='solid' mb='1rem' top={['10px', '25px', '25px', '15px']}>
                 Back to Home
             </Button>
         </Link>
         </Box>
 
-        <Box alignSelf='center' justifyContent='center' bgGradient='linear(170deg, black, #063970, black)' borderRadius='var(--chakra-radii-lg)'
-        width={['45%','45%','30%','30%']} color='white' height={['2rem', '2rem', '4rem', '4rem']} padding={['1','1','2','2']}>
-            <Text fontSize={['1rem', '1rem', '2rem', '2rem']} textAlign='center'>Booking History</Text>
+        <Box alignSelf='center' justifyContent='center' bgColor="#063970"  borderRadius='var(--chakra-radii-lg)'
+        width={['45%','45%','35%','30%']} color='white' height={['2rem', '2rem', '4rem', '4rem']} padding={['1','1','2','2']}>
+            <Text fontSize={['1rem', '1rem', '2rem', '2rem']} textAlign='center' color='ffffff'>Booking History</Text>
         </Box>
     </Flex>
       <Flex flexDirection='column' height='100vh' widht='100%' padding='8'>
         <CardHistorybook />
-        <CardSearchs />
+        
+        {/* <CardSearchs /> */}
       </Flex>
+      <Box>
+        <Image src={wave} widht='100%' pos='absolute'/>
+        <Image src={wave2} widht='100%'/>
+      </Box>
     </Box>
   );
 }
