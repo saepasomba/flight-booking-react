cd951d60ef0d6977dd196a3082f9eb1d935c7458
MhassanS23 <maulanahassan366@gmail.com>
2022-12-27 17:30:45 +0700

    add qr code in history card

--- src/components/card/cardHistorybook.js
+++ src/components/card/cardHistorybook.js
@@ -1,5 +1,6 @@
 import {
     Flex,
+    Box,
     Text,
     Heading,
     Divider,
@@ -9,6 +10,16 @@ import {
     Center,
     Spinner,
     MenuItem,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+    Button,
   } from '@chakra-ui/react'
   import React, {useState, useEffect} from 'react'
 import { Card, CardHeader, CardBody} from '@chakra-ui/react'
@@ -79,7 +90,7 @@ export default function CardHistorybook() {
     </Flex>
 </Card>
     
-    ) : history.length > 0 ? (
+    ) : history && history.length > 0 ? (
     history.map((histories)=>{
         return(
             <Card padding='0' width={['95%','80%', '80%', '75%']} bg='white' border='1px solid whitesmoke' alignSelf='center' borderRadius='var(--chakra-radii-xl)' mb='1rem' size='sm' >
@@ -104,9 +115,26 @@ export default function CardHistorybook() {
                         <FaCalendarAlt size='1.5rem' color='black'/>
                         <Text> {histories.paymentDate} </Text>
                     </Flex>
-                    <Flex gap='3' mb='0.5rem'>
-                        <GiAchievement size='1.5rem' color='black'/>
-                        <Text > {histories.classType} </Text>
+                    <Flex gap='3' mb='0.5rem' flexDirection={['column','column','row', 'row']} justifyContent={['center','center','space-between','space-between']}>
+                        <Flex gap='3' mb='0.5rem'>
+                            <GiAchievement size='1.5rem' color='black'/>
+                            <Text > {histories.classType} </Text>
+                        </Flex>
+                            <Popover>
+                                <PopoverTrigger>
+                                    <Button color='white' bg='#3579e6' cursor='pointer'>Show QR Code</Button>
+                                </PopoverTrigger>
+                                <PopoverContent widht='var(--chakra-sizes-64)'>
+                                    <PopoverArrow />
+                                    <PopoverCloseButton />
+                                    <PopoverHeader color='black' textAlign='center' _hover={{background: '#063970', color:'white'}}>QR CODE</PopoverHeader>
+                                    <PopoverBody>
+                                        <Center>
+                                            <Image widht='50%' src={histories.qrCodeUrl}/>
+                                        </Center>
+                                    </PopoverBody>
+                                </PopoverContent>
+                            </Popover>
                     </Flex>
                     
                 </CardBody>
@@ -126,31 +154,11 @@ export default function CardHistorybook() {
         )
     })
     ) : (
-        <Card padding='0' width={['95%','80%', '80%', '75%']} bg='white' border='1px solid whitesmoke' alignSelf='center' borderRadius='var(--chakra-radii-xl)' mb='1rem' size='sm' >
-            <CardHeader bg='#063970' color='white' borderTopLeftRadius='var(--chakra-radii-xl)' borderTopRightRadius='var(--chakra-radii-xl)' pb='3'>
-                <Flex flexDirection={['column','column','row', 'row']} justifyContent={['center','center','space-between','space-between']} >
-                <Skeleton height='1.2rem' width='12rem' size={['sm','sm','md','md']} mb={['0.5rem','0.5rem','0rem','0rem']}/>
-                <Skeleton height='1.2rem' width='7rem' size={['sm','sm','md','md']}/>
-                </Flex>
-            </CardHeader>
-  
-            <CardBody padding='4' color='blue'  bg='#3579e6'>
-                <Center>
-                    <Spinner
-                    thickness='4px'
-                    speed='0.65s'
-                    emptyColor='gray.200'
-                    color='blue.500'
-                    size='xl'
-                    />
-                </Center>
-            </CardBody>
-  
-            <Flex flexDirection={['column','column','row', 'row']} justifyContent={['center','center','space-between','space-between']} padding='1rem' color='white'  alignItems={['flex-end','flex-end','center','center']} bg='#063970' borderBottomLeftRadius='var(--chakra-radii-xl)' borderBottomRightRadius='var(--chakra-radii-xl)'>
-                <Skeleton height='1.2rem' width='5rem' size={['sm','sm','md','md']} mb={['0.5rem','0.5rem','0rem','0rem']}/>
-                <Skeleton height='1.2rem' width='9rem' size={['sm','sm','md','md']} />
-            </Flex>
-        </Card>
+        <Box alignSelf='center'>
+            <Text color='#063970'>
+                <h1 color>History Booking Not Found</h1>
+            </Text>
+        </Box>
     )
 }
 
