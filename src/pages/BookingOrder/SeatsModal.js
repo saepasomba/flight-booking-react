d483ee0971e166d09434519d44e11eb6fd53909f
Sae Pasomba <saepasomba12@gmail.com>
2022-12-28 17:45:46 +0700

    Add GUI for selecting seats

---
+++ src/pages/BookingOrder/SeatsModal.js
@@ -0,0 +1,68 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+
+export default function SeatsModal({
+  isOpen,
+  onClose,
+  seatsData,
+  setValue,
+  index,
+  setSeatSelected,
+}) {
+  const seatClicked = (seat) => {
+    setValue(`details.${index}.idSeats`, seat.seatsId);
+    setSeatSelected(seat.seatsNumber);
+    onClose();
+  };
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Choose seat for passenger {index + 1}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Flex justifyContent="space-around">
+            {seatsData.map((group) => {
+              return (
+                <Flex flexDir="column" alignItems="center" gap="0.5rem">
+                  <Heading>{group.groupSeats}</Heading>
+                  {group.seatsList.map((seat) => {
+                    return (
+                      <Button
+                        w="100%"
+                        colorScheme={
+                          seat.statusSeats === "AVAILABLE" && seat.canBook
+                            ? "blueHue"
+                            : "gray"
+                        }
+                        disabled={
+                          seat.statusSeats === "AVAILABLE" && seat.canBook
+                            ? false
+                            : true
+                        }
+                        onClick={() => seatClicked(seat)}
+                      >
+                        {seat.seatsNumber}
+                      </Button>
+                    );
+                  })}
+                </Flex>
+              );
+            })}
+          </Flex>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
